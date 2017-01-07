bc224c72275fbcd7843c9cc76d72b6fa31bbfd55
gulkin-nos <gulkin-nos@ya.ru>
2017-01-07 16:25:42 +0300

    Initial commit

--- index.php
+++ index.php
@@ -1,9 +1,23 @@
 <?php
+header('Content-Type: text/html; charset=utf-8');
+?>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Мой проект на новогодние праздники</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <script src="js/jquery-3.1.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</head>
 
-$var1=1;
-$var2=2;
-$var3=1;
-$var4=1;
-phpinfo();
 
-die(var_dump('happy NY'));
+<body>
+    <div class="row">
+    </div>
+    <p><span class="glyphicon glyphicon-wrench"></span>сайт находится в стадии разработки..</p>
+<?php
+//phpinfo();
+?>
+</body>
+
